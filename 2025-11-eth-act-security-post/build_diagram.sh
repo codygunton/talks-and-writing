c488b85f0e07edb64ab4d2065cb306d6c1645254
Cody Gunton <codygunton@gmail.com>
2025-12-11 16:20:02 -0500

    use svg with background

--- 2025-11-eth-act-security-post/build_diagram.sh
+++ 2025-11-eth-act-security-post/build_diagram.sh
@@ -3,4 +3,5 @@ set -eu
 
 cd assets
 pdflatex pipeline.tex
-pdf2svg pipeline.pdf pipeline.svg
+pdf2svg pipeline.pdf pipeline_no_bg.svg
+sed 's|<svg \([^>]*\)>|<svg \1>\n<rect width="100%" height="100%" fill="white"/>|' pipeline_no_bg.svg > pipeline.svg
